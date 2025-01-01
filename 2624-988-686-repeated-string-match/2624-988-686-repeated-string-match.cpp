fb2308a110ee60b8049004bdf43d1fe88f07e0bf
PRIYASAKI <125540653+PRIYASAKI@users.noreply.github.com>
2025-01-01 21:13:07 +0530

    Time: 7 ms, Memory: 9.8 MB - LeetHub

--- 2624-988-686-repeated-string-match/2624-988-686-repeated-string-match.cpp
+++ 2624-988-686-repeated-string-match/2624-988-686-repeated-string-match.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        int len_a=a.length();
-        int len_b=b.length();
-        int n=len_b/len_a;
-        int cnt=n;
+    int repeatedStringMatch(string A, string B) {
+        int count=0;
+        string s="";
+        while(s.size()<B.size())
+        {
+            s+=A;
+            count++;
+        }
+        if(s.find(B)!=string::npos)
+            return count;
 
-        string na="";
-        while(cnt--)
-        {
-            na=na+a;
-        }
-        if(na.find(b)!=std::string::npos)
-        {
-            return n;
-        }
-        na=na+a;
-        if(na.find(b)!=std::string::npos)
-        {
-            return n+1;
-        }
-        na=na+a;
-        if(na.find(b)!=std::string::npos)
-        {
-            return n+2;
-        }
-      return -1;
-        
+        s+=A;
+        count++;
+         if(s.find(B)!=string::npos)
+            return count;
+    return -1;
     }
 };
