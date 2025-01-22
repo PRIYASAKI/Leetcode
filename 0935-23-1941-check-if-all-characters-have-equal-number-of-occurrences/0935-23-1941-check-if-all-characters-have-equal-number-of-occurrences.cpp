e99c8b2b8e7a195aee03ac4b43620899b2d981ed
PRIYASAKI <125540653+PRIYASAKI@users.noreply.github.com>
2025-01-22 19:41:55 +0530

    Time: 0 ms, Memory: 9.6 MB - LeetHub

---
+++ 0935-23-1941-check-if-all-characters-have-equal-number-of-occurrences/0935-23-1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        unordered_map<char,int>mp;
+        for(auto i : s)
+        {
+            mp[i]++;
+        }
+        for(int i=0;i<s.size()-1;i++)
+        {
+            if(mp[s[i]]!=mp[s[i+1]])
+            { 
+                return false;
+            }
+        }
+    return true;
+        
+    }
+};
